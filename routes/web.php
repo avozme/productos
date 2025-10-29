92398e8cc3bfeeed5e6230bcec07908b5e807b97
avozme <avozme@yahoo.com>
2025-10-29 18:05:18 +0100

    Agrego .env al repo

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,13 @@ use App\Http\Controllers\ProductController;
 
 
 Route::get('/', [ProductController::class, 'index']);
-Route::resource('products', ProductController::class);
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+// Las 7 rutas anteriores pueden resumirse con solo esta línea:
+// Route::resource('products', ProductController::class);
